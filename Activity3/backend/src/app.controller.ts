3965a2dd4fc79655491ef03cd5126ab85cf290a4
kylee05 <ronmarguerrero@gmail.com>
2025-11-02 17:13:48 +0800

    Delete Activity3 directory

--- Activity3/backend/src/app.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller()
-export class AppController {
-  @Get()
-  getHello() {
-    return { message: '📚 Bookshelf API is running!' };
-  }
-}
