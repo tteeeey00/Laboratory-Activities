2369a5b9ea8ce49622d403f5a5bf5d9a5054a685
SiestA <ic.johnferdinand.valdez@cvsu.edu.ph>
2025-11-03 02:26:02 +0800

    Delete Activity5 directory

--- Activity5/backend/src/comments/dto/update-comment.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString } from 'class-validator';
-
-export class UpdateCommentDto {
-  @ApiProperty({ example: 'Updated comment content', description: 'Comment content' })
-  @IsString()
-  @IsNotEmpty()
-  content: string;
-}
